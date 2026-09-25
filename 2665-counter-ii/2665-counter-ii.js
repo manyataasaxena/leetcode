041631a494e0c91df6461e9695642c2c4b2a0947
manyata saxena <168563806+manyataasaxena@users.noreply.github.com>
2026-09-25 20:59:29 +0530

    Time: 53 ms (38.34%), Space: 56.4 MB (58.1%) - LeetHub

---
+++ 2665-counter-ii/2665-counter-ii.js
@@ -0,0 +1,9 @@
+var createCounter = function(init) {
+    let count = init;
+
+    return {
+        increment: () => ++count,
+        decrement: () => --count,
+        reset: () => count = init
+    };
+};
